d6f4780bd19cf6d2d31a1dbd62d4276d246639d5
stud <stud@almetpt.local>
2022-12-28 10:04:17 +0300

    code change

--- app/src/main/java/com/example/bestquizzes/ui/MainActivity2.kt
+++ app/src/main/java/com/example/bestquizzes/ui/MainActivity2.kt
@@ -2,10 +2,61 @@ package com.example.bestquizzes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.bestquizzes.R
+import com.example.bestquizzes.databinding.ActivityMainBinding
+import com.example.bestquizzes.ui.addQuizz.AddQuizzFragment
+import com.example.bestquizzes.ui.filtr.FilterFragment
+import com.example.bestquizzes.ui.general.GeneralFragment
+import com.example.bestquizzes.ui.profile.PersonFragment
+import com.example.bestquizzes.ui.settings.SettingsFragment
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity2 : AppCompatActivity() {
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    val uid = auth.currentUser?.uid
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadFragment(GeneralFragment())
+
+        binding.bottomNavigation.setOnItemSelectedListener{ item ->
+
+            when (item.itemId){
+                R.id.homeNavigation -> {
+                    loadFragment(GeneralFragment())
+                    true
+                }
+                R.id.filterNavigation -> {
+                    loadFragment(FilterFragment())
+                    true
+                }
+                R.id.settingNavigation -> {
+                    loadFragment(SettingsFragment())
+                    true
+                }
+                R.id.addNavigation -> {
+                    loadFragment(AddQuizzFragment())
+                    true
+                }
+                R.id.personNavigation -> {
+                    loadFragment(PersonFragment())
+                    true
+                }
+                else -> false
+            }
+
+        }
     }
+    private fun loadFragment(fragment: Fragment) {
+        // load fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
 }
